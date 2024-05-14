95a3d9f7cbb67b46ad737d1c9a942de5ffd404b4
YanLuLin <163007489+YanLuLin@users.noreply.github.com>
2024-05-15 00:13:55 +0800

    Update GAOR_eg.m

--- GAOR_eg.m
+++ GAOR_eg.m
@@ -1,4 +1,5 @@
 function [opt_omega,opt_gamma,opt_IT,opt_cputime,res]=GAOR_eg(u,m,maxit,omega_0,omega_N,delta_omega,gamma_0,gamma_N,delta_gamma)
+n=m^2;
 eta=1.0e-6;
 [A,b,xk] = eg_3(m);
 N_omega=floor((omega_N-omega_0)/delta_omega);       
@@ -8,7 +9,7 @@ for j=1:N_omega
     omega(j)=omega_0+j*delta_omega;
     for k=1:N_gamma
         gamma(j,k)=gamma_0+k*delta_gamma;
-        [IT(j,k),cputime(j,k),res(j,k),omega(j),gamma(j,k)]=GAOR(A,b,xk,u,omega(j),gamma(j,k),eta,maxit);
+        [IT(j,k),cputime(j,k),res(j,k),omega(j),gamma(j,k)]=GAOR(n,A,b,xk,u,omega(j),gamma(j,k),eta,maxit);
         y=[IT(j,k),cputime(j,k),res(j,k),omega(j),gamma(j,k)];
         H=[H;y];
     end

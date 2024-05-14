252e65222f839a870972cc4a3b2018a36d32ec57
YanLuLin <163007489+YanLuLin@users.noreply.github.com>
2024-05-15 00:17:02 +0800

    Update TPRI_eg_qd.m

--- TPRI_eg_qd.m
+++ TPRI_eg_qd.m
@@ -1,4 +1,5 @@
 function [omega,gamma,tau,IT,CPU,RES]=TPRI_eg_qd(m,maxit,omega,gamma,tau)
+n=m^2;
 eta=1.0e-6;
 [A,b,xk] = eg_3(m);
-[IT,CPU,RES,omega,gamma,tau]=TPRI(A,b,xk,omega,gamma,tau,eta,maxit); 
+[IT,CPU,RES,omega,gamma,tau]=TPRI(n,A,b,xk,omega,gamma,tau,eta,maxit); 

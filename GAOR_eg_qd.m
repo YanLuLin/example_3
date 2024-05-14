f5a498ebf283e8d797fa059d603b1a1383fcbcb7
YanLuLin <163007489+YanLuLin@users.noreply.github.com>
2024-05-15 00:14:32 +0800

    Update GAOR_eg_qd.m

--- GAOR_eg_qd.m
+++ GAOR_eg_qd.m
@@ -1,4 +1,5 @@
 function [omega,gamma,IT,cputime,res]=GAOR_eg_qd(u,m,maxit,omega,gamma)
+n=m^2;
 tol=1.0e-6;
 [A,b,xk] = eg_3(m);
-[IT,cputime,res]=GAOR(A,b,xk,u,omega,gamma,tol,maxit); 
+[IT,cputime,res]=GAOR(n,A,b,xk,u,omega,gamma,tol,maxit); 
